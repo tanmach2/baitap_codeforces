ad1476593e7d006d276f86b27119c532d0f9a184
tan <phamtan160499@gmail.com>
2020-08-20 18:00:20 +0700

    Resolve conflict

--- 282A.py
+++ 282A.py
@@ -10,3 +10,11 @@ for v in list:
     elif v == "--X" or v == "X--":
         k -= 1
 print (k)
+# thuật toán:
+# b1: nhập số nguyên s.
+# b2: kiểm tra điều kiện 1<= s <=150:
+# b3: nếu nằm trong khoảng thì thực hiện nhập các ký tự ở dòng 2 vào trong list.
+# b4: kiểm tra các phần trong list:
+#   - nếu phần tử đó là "X++" hoặc "++X" thì tăng biến k = 0 lên một đơn vị.
+#   - nếu phần tử đó là "--X" hoặc "X--" thì giảm biến k đi 1 đơn vị. 
+# b5: in ra kết quả.
