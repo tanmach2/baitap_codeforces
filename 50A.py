13d2f6aa34858a8ad4b988e270b579499e0e8ea6
tan <phamtan160499@gmail.com>
2020-08-20 17:42:00 +0700

    add 50A.py file

--- 50A.py
+++ 50A.py
@@ -7,3 +7,12 @@ if int(lst[1]) % 2 == 0:
 else:
     k = ((int(lst[1]) - 1) / 2 ) * int(lst[0]) + int(lst[0]) / 2
     print(int(k))
+
+# input: chiều dài và chiều rộng của khung hcn.(m,n cùng trên 1 dòng cách nhau bằng khoảng trắng)
+# output: số dominal cho vaog được trong khung hcn.  
+# thuật toán.
+    # b1: nhập chiều dài , chiều rộng.
+    # b2: biến dòng vừa nhập thành 1 list sử dụng hàm split(" ").
+    # b3: kiểm tra xem chiều dài của khung là só chẵn hay số lẻ.
+    # b4: nếu chiều dài là chẵn: thì số dominal  = (chiều dài/2)*chiều rộng. sau đó in ra kết quả.
+    # b5: nếu chiều dài là số lẻ: thì số dominal = ((chiều dài-1)/2)* chiều rộng + (chiều rộng/2). sau đó in ra kết quả.
