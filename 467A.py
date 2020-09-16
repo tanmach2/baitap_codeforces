818abdaaa10c4da43d763c4ab75ff51017455457
tan <phamtan160499@gmail.com>
2020-09-16 14:49:14 +0700

    add file

--- 467A.py
+++ 467A.py
@@ -3,6 +3,6 @@ count = 0
 for i in range(s):
     lst  = input()
     lst1 = lst.split(" ")
-    if int(lst1[0]) < int(lst1[1]):
+    if int(lst1[0]) < int(lst1[1]) and int(lst1[1]) - int(lst1[0]) >= 2:
         count +=1
 print(count)
